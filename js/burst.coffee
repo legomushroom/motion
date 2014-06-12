66190c0b473bcb8320190d7c95f5616a44439d8d
sol0mka <legomushroom@gmail.com>
2014-06-12 22:29:22 +0300

    amend

--- js/burst.coffee
+++ js/burst.coffee
@@ -11,15 +11,50 @@ class Burst extends motion.Bit
 
   init:()->
     @add2Dom(); @
+
+    
   
   vars:->
     super
+    @radius ?= @o.radius or 80
+    @cnt     = @o.cnt-1
+    @step = (1*Math.PI)/(@cnt+1)
+    @rotateStep = 180/(@cnt+1)
+
     @cloneBits
       class: 'bit'
       cnt: @cnt
     @$el.css
       width:  2*@o.radius or 200
       height: 2*@o.radius or 200
+
+    @$el.velocity('stop')
+      .velocity
+        rotateZ: @o.initialRotation or 0
+      ,
+        duration: 1
+
+    rotateAngle = 0
+    angle = 0
+    centerX = 0
+    centerY = 0
+    for $el, i in @$els
+      left  = parseInt(centerX+(Math.cos(angle)*(@radius)),10)
+      top   = parseInt(centerY+(Math.sin(angle)*(@radius)),10)
+      $el.velocity('stop')
+        .velocity
+          translateX:  left/2
+          translateY:  top/2
+          marginTop: 0
+          rotateZ: rotateAngle+90
+          height: 20
+        ,
+          duration: 1
+      rotateAngle += @rotateStep
+      angle += @step
+
+
+
 
   animate:(o)->
     console.log o.radius
@@ -64,10 +99,7 @@ class Burst extends motion.Bit
           duration: o?.rotationDuration or 900
 
   reset:->
-    @radius ?= @o.radius or 80
-    @cnt     = @o.cnt-1
-    @step = (2*Math.PI)/(@cnt+1)
-    @rotateStep = 360/(@cnt+1)
+    
 
     # @$el.css
     #   transform: "translateX(#{@o.left - @radius}px) translateY(#{@o.top - @radius}px)" 
--- js/burst.js
+++ js/burst.js
@@ -18,15 +18,50 @@
     };
 
     Burst.prototype.vars = function() {
+      var $el, angle, centerX, centerY, i, left, rotateAngle, top, _i, _len, _ref, _results;
       Burst.__super__.vars.apply(this, arguments);
+      if (this.radius == null) {
+        this.radius = this.o.radius || 80;
+      }
+      this.cnt = this.o.cnt - 1;
+      this.step = (1 * Math.PI) / (this.cnt + 1);
+      this.rotateStep = 180 / (this.cnt + 1);
       this.cloneBits({
         "class": 'bit',
         cnt: this.cnt
       });
-      return this.$el.css({
+      this.$el.css({
         width: 2 * this.o.radius || 200,
         height: 2 * this.o.radius || 200
       });
+      this.$el.velocity('stop').velocity({
+        rotateZ: this.o.initialRotation || 0
+      }, {
+        duration: 1
+      });
+      rotateAngle = 0;
+      angle = 0;
+      centerX = 0;
+      centerY = 0;
+      _ref = this.$els;
+      _results = [];
+      for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+        $el = _ref[i];
+        left = parseInt(centerX + (Math.cos(angle) * this.radius), 10);
+        top = parseInt(centerY + (Math.sin(angle) * this.radius), 10);
+        $el.velocity('stop').velocity({
+          translateX: left / 2,
+          translateY: top / 2,
+          marginTop: 0,
+          rotateZ: rotateAngle + 90,
+          height: 20
+        }, {
+          duration: 1
+        });
+        rotateAngle += this.rotateStep;
+        _results.push(angle += this.step);
+      }
+      return _results;
     };
 
     Burst.prototype.animate = function(o) {
@@ -86,12 +121,6 @@
 
     Burst.prototype.reset = function() {
       var $el, angle, centerX, centerY, i, left, rotateAngle, top, _i, _len, _ref, _results;
-      if (this.radius == null) {
-        this.radius = this.o.radius || 80;
-      }
-      this.cnt = this.o.cnt - 1;
-      this.step = (2 * Math.PI) / (this.cnt + 1);
-      this.rotateStep = 360 / (this.cnt + 1);
       this.$el.velocity('stop').velocity({
         rotateZ: this.o.initialRotation || 0
       }, {
